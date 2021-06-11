373c36b678468f760bf067a782a3cc609910ca96
Jacek Szmelter <jacek.szmelter.ext@qonto.com>
2021-06-11 12:43:27 +0200

    Clean up the file.

--- app/src/main/java/com/jsz/collapsingtoolbardemo/CollapsingToolbarLayoutExt.kt
+++ app/src/main/java/com/jsz/collapsingtoolbardemo/CollapsingToolbarLayoutExt.kt
@@ -7,7 +7,17 @@ import com.google.android.material.appbar.AppBarLayout.LayoutParams.*
 import com.google.android.material.appbar.AppBarLayout.OnOffsetChangedListener
 import com.google.android.material.appbar.CollapsingToolbarLayout
 
-fun CollapsingToolbarLayout.enableScroll() {
+fun CollapsingToolbarLayout.adoptScrollingFlagsBasedOn(scrollView: NestedScrollView) {
+    scrollView.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
+        if (scrollView.canScroll()) {
+            enableScroll()
+        } else {
+            expandAndDisableScroll()
+        }
+    }
+}
+
+private fun CollapsingToolbarLayout.enableScroll() {
     val layoutParams = layoutParams as LayoutParams
     layoutParams.scrollFlags = SCROLL_FLAG_SCROLL or
             SCROLL_FLAG_EXIT_UNTIL_COLLAPSED or
@@ -15,30 +25,20 @@ fun CollapsingToolbarLayout.enableScroll() {
     this.layoutParams = layoutParams
 }
 
-fun CollapsingToolbarLayout.disableScroll() {
-    val layoutParams = layoutParams as LayoutParams
-    layoutParams.scrollFlags = SCROLL_FLAG_NO_SCROLL
-    this.layoutParams = layoutParams
-}
-
-fun CollapsingToolbarLayout.adoptScrollingFlagsBasedOn(scrollView: NestedScrollView) {
-    scrollView.addOnLayoutChangeListener { _, _, _, _, _, _, _, _, _ ->
-        if (scrollView.canScroll()) {
-            enableScroll()
-        } else {
-            expand()
-        }
-    }
-}
-
-fun CollapsingToolbarLayout.expand() {
+private fun CollapsingToolbarLayout.expandAndDisableScroll() {
     (parent as? AppBarLayout)?.let {
         it.addOnOffsetChangedListener(onExpandedListener { disableScroll() })
         it.setExpanded(true, true)
     }
 }
 
-fun onExpandedListener(onExpandedBlock: (() -> Unit)) = object : OnOffsetChangedListener {
+private fun CollapsingToolbarLayout.disableScroll() {
+    val layoutParams = layoutParams as LayoutParams
+    layoutParams.scrollFlags = SCROLL_FLAG_NO_SCROLL
+    this.layoutParams = layoutParams
+}
+
+private fun onExpandedListener(onExpandedBlock: (() -> Unit)) = object : OnOffsetChangedListener {
     override fun onOffsetChanged(appBarLayout: AppBarLayout, verticalOffset: Int) {
         if (verticalOffset == 0) {
             onExpandedBlock()
@@ -46,30 +46,3 @@ fun onExpandedListener(onExpandedBlock: (() -> Unit)) = object : OnOffsetChanged
         }
     }
 }
-
-//abstract class AppBarStateChangeListener : OnOffsetChangedListener {
-//
-//    enum class State {
-//        EXPANDED, COLLAPSED, IDLE
-//    }
-//
-//    private var mCurrentState = State.IDLE
-//    override fun onOffsetChanged(appBarLayout: AppBarLayout, i: Int) {
-//        mCurrentState = when {
-//            i == 0 -> {
-//                if (mCurrentState != State.EXPANDED) onStateChanged(appBarLayout, State.EXPANDED)
-//                State.EXPANDED
-//            }
-//            abs(i) >= appBarLayout.totalScrollRange -> {
-//                if (mCurrentState != State.COLLAPSED) onStateChanged(appBarLayout, State.COLLAPSED)
-//                State.COLLAPSED
-//            }
-//            else -> {
-//                if (mCurrentState != State.IDLE) onStateChanged(appBarLayout, State.IDLE)
-//                State.IDLE
-//            }
-//        }
-//    }
-//
-//    abstract fun onStateChanged(appBarLayout: AppBarLayout, state: State)
-//}
